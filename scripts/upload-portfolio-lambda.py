f81a8fe1c4f8668706550608ca943254fb5e6dff
matthewmagee05 <matthewmagee05@gmail.com>
2017-10-12 20:14:44 -0800

    Use bucket name and object key from codepipeline job done

--- scripts/upload-portfolio-lambda.py
+++ scripts/upload-portfolio-lambda.py
@@ -3,17 +3,28 @@ import StringIO
 import zipfile
 import mimetypes
 
-exports.handler = (event, context, callback) => {
+def lambda_handler(event, context):
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:us-east-1:114064149276:deployPortfolioTopic')
+    location = {
+        "bucketName": "portfoliobuild.mmagee.com",
+        "objectKey": "portfoliobuild.zip"
+    }
     
     try:
+        job = event.get("CodePipeline.job")
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                if artifact["name"] == "MyAppBuild":
+                    location = artifact["location"]["s3Location"]
+                    
+        print "Building portfolio from " + str(location)
         s3 = boto3.resource('s3')
         portfolio_bucket = s3.Bucket('portfolio.mmagee.com')
-        build_bucket = s3.Bucket('portfoliobuild.mmagee.com')
+        build_bucket = s3.Bucket(location["bucketName"])
         portfolio_zip = StringIO.StringIO()
     
-        build_bucket.download_fileobj('portfoliobuild.zip',portfolio_zip)
+        build_bucket.download_fileobj(location["objectKey"],portfolio_zip)
     
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for nm in myzip.namelist():
@@ -22,9 +33,12 @@ exports.handler = (event, context, callback) => {
                 portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
         print "Job done!"
         topic.publish(Subject="Portfolio Deployed", Message="Portfolio deployed successfully")
+        
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job["id"])
     except:
-        topic.publish(Subject="Portfolio Deploy Failed", Message"The Portfolio was not deployed successfully!")
+        topic.publish(Subject="Portfolio Deploy Failed", Message="The Portfolio was not deployed successfully!")
         raise
     
-    callback(null, 'Hello from Lambda');
-};
+    return 'Hello from Lambda'
